8d08df23fcae4d3c81dad34563a4ff0e93581e94
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-17 10:50:50 +0200

    init srp

---
+++ SRPExample/SRPExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SRPExample
+//
+//  Created by Mykhailo Bondarenko on 17.02.2020.
+//  Copyright © 2020 Mykhailo Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
