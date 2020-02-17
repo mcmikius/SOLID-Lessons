43ea950b9eee45cae0066f7e0e1a208069a9de4d
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-17 11:04:58 +0200

    start SRP

--- SRPExample/SRPExample/ViewController.swift
+++ SRPExample/SRPExample/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        saveButton.layer.cornerRadius = saveButton.frame.width / 2
     }
-
+    
+    func changeName() {
+        guard let name = myLabel.text, name != "" else {
+            showAlert()
+            return
+        }
+        print("Your name: \(name)")
+    }
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "WARNING", message: "YOUR NAME CAN'T BE EMPTY.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    @IBAction func changeLabel(_ sender: UITextField) {
+        myLabel.text = myTextField.text
+    }
+    @IBAction func svaeButtonTapped(_ sender: UIButton) {
+        changeName()
+    }
+    
 
 }
 
