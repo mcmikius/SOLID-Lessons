642239b33656ef65926e6193a2c1866461273cfe
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-17 11:22:40 +0200

    massiveVC

--- SRPExample/SRPExample/ViewController.swift
+++ SRPExample/SRPExample/ViewController.swift
@@ -8,7 +8,16 @@
 
 import UIKit
 
+struct Country: Decodable {
+    var id: String
+    var time: String
+    var name: String
+    var image: String?
+}
+
 class ViewController: UIViewController {
+    
+    let urlString = "https://raw.githubusercontent.com/Softex-Group/task-mobile/master/test.json"
 
     @IBOutlet weak var myTextField: UITextField!
     @IBOutlet weak var myLabel: UILabel!
@@ -18,6 +27,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         saveButton.layer.cornerRadius = saveButton.frame.width / 2
+        dataFetcher()
     }
     
     func changeName() {
@@ -26,6 +36,39 @@ class ViewController: UIViewController {
             return
         }
         print("Your name: \(name)")
+    }
+    
+    // MARK: - Data storage
+    
+    func saveNameInCache(name: String) {
+        print("Your name: \(name) is saved")
+    }
+     
+    func getNameFromCache() -> String {
+        return "some name"
+    }
+    
+    // MARK: - Networking
+    
+    func dataFetcher() {
+        request { (data, error) in
+            let decoder = JSONDecoder()
+            guard let data = data else {return}
+            let response = try? decoder.decode([Country].self, from: data)
+            print(response)
+        }
+    }
+    
+    func request(completion: @escaping (Data?, Error?) -> Void) {
+        guard let url = URL(string: urlString) else { return }
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            DispatchQueue.main.async {
+                completion(data, error)
+            }
+            
+        }
+        task.resume()
     }
     
     func showAlert() {
